d48977209145aa1cf585b647b83ee53c68d9fd3f
Richard NSHIMYUMUKIZA <ricahcyuzuzo@gmail.com>
2022-12-06 11:04:23 +0200

    Adding pictures

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -12,15 +12,20 @@ const Products = () => {
     const [loading, setLoading] = useState(false);
 
 
-    const sendOrder = () => {
+    const sendOrder = async () => {
         setLoading(true)
-        axios.post('https://amizerowhatsappapi.onrender.com/send', { 
+        await axios.post('https://amizerowhatsappapi.onrender.com/send', { 
             phone: "+250781064092",
             body: `You have got an order from ${name}, with ${quantity} at a price of ${selectedProduct?.price} which is total of $${price}.\nThe contacts are: ${contacts}`
-        }).then(() => {
-            setLoading(false);
-            alert('Your order has been sent successfully, We will reply to as soon as possible. thank you.')
         })
+        
+        await axios.post('https://amizerowhatsappapi.onrender.com/send', { 
+            phone: "+250782399504",
+            body: `You have got an order from ${name}, with ${quantity} at a price of ${selectedProduct?.price} which is total of $${price}.\nThe contacts are: ${contacts}`
+        });
+            
+        alert('Your order has been sent successfully, We will reply to as soon as possible. thank you.')
+        setLoading(false);
     }
 
     const products = [
@@ -28,58 +33,111 @@ const Products = () => {
             category: "Bags",
             items: [
                 {
-                    name: 'Beach bag',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Beach-bag-300x300.png',
-                    price: 10
+                    name: 'Women set bags',
+                    image: require('./assets/bag1.jpeg'),
+                    price: 49
                 },
                 {
-                    name: 'Carry bag',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Carry-bag-300x300.png',
-                    price: 10
-
+                    name: 'Elephant bag for kids',
+                    image: require('./assets/bag2.jpeg'),
+                    price: 31
                 },
                 {
-                    name: ' Clutch Bag in blue',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Clutch-bag-in-blue-300x300.png',
-                    price: 10
-
+                    name: 'Shopping bag',
+                    image: require('./assets/bag3.jpeg'),
+                    price: 30
                 },
                 {
-                    name: 'Clutch bag in orage',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Clutch-bag-in-orange-300x300.png',
-                    price: 10
+                    name: 'I Pad bag',
+                    image: require('./assets/bag4.jpeg'),
+                    price: 20
                 },
                 {
-                    name: 'Elephant bag in red',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Ellephant-backpack-in-red-300x300.png',
-                    price: 10
+                    name: 'Shopping bag',
+                    image: require('./assets/bag5.jpeg'),
+                    price: 31
+                },
+            ]
+        },
+        {
+            category: "Clothes",
+            items: [
+                {
+                    name: 'Women Dress',
+                    image: require('./assets/cloth1.jpeg'),
+                    price: 28
                 },
                 {
-                    name: 'Embroidery Clutch Bag in blue',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Embroidery-clutch-bag-bird-300x300.png',
-                    price: 10
+                    name: 'Woman Dress',
+                    image: require('./assets/cloth2.jpeg'),
+                    price: 35
                 },
                 {
-                    name: 'Embroidery red bag',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Clutch-bag-in-orange-300x300.png',
-                    price: 10
+                    name: 'Women Jackeck',
+                    image: require('./assets/cloth3.jpeg'),
+                    price: 47
                 },
                 {
-                    name: 'Elephant bag in red',
-                    image: 'https://thefoundation4hope.com/wp-content/uploads/2020/02/Ellephant-backpack-in-red-300x300.png',
-                    price: 10
+                    name: 'Kid Dress',
+                    image: require('./assets/cloth4.jpeg'),
+                    price: 20
+                },
+                {
+                    name: 'Men Jacket',
+                    image: require('./assets/cloth5.jpeg'),
+                    price: 31
+                },
+            ]
+        },
+        {
+            category: "Articrafts",
+            items: [
+                {
+                    name: 'Backet',
+                    image: require('./assets/art1.jpeg'),
+                    price: 46
+                },
+                {
+                    name: 'Elepfant stuffed animal',
+                    image: require('./assets/art2.jpeg'),
+                    price: 30
+                },
+                {
+                    name: 'Plateau Baskets',
+                    image: require('./assets/art3.jpeg'),
+                    price: 61
+                },
+                {
+                    name: 'Plateau & Trad Basket',
+                    image: require('./assets/art4.jpeg'),
+                    price: 63
+                },
+                {
+                    name: 'Purse Basket',
+                    image: require('./assets/art5.jpeg'),
+                    price: 36
+                },
+                {
+                    name: 'Colored Cup Basket',
+                    image: require('./assets/art6.jpeg'),
+                    price: 37
+                },
+                {
+                    name: 'Hut Basket',
+                    image: require('./assets/art8.jpeg'),
+                    price: 39
                 },
             ]
         }
     ]
   return (
     <div className='w-full h-fit bg-[#f9e9e0] pb-[80px]'>
-        <div className='w-[90%] md:w-[80%] mx-auto'>
+        <div className='w-[90%] md:w-[70%] mx-auto'>
             <h1 className='text-center text-[30px] font-extrabold text-[#383838] pt-[30px]'>Our Products</h1>
             <div className='w-full mt-[100px]'>
                 {
                     products.map((item, idx) => (
-                        <div className='flex flex-col gap-5 cursor-pointer' key={idx}>
+                        <div className='flex flex-col gap-5 mt-[70px]' key={idx}>
                             <h1 className='text-[24px] text-[#4D335E] font-medium text-center underline leading-9 mb-[40px]'>{item.category}</h1>
                             <div className='flex gap-6 flex-col md:flex-row flex-wrap'>
                                 {
@@ -89,8 +147,8 @@ const Products = () => {
                                                 setSelectedProduct(item);
                                                 setModalVisible(true);
                                                 setPrice(item.price);
-                                            }} className='bg-[#dec9e9] rounded-2xl flex flex-col justify-center items-center '>
-                                                <img src={item.image} />
+                                            }} className='bg-[#dec9e9] rounded-2xl flex flex-col justify-center cursor-pointer items-center '>
+                                                <img src={item.image} className='w-[200px] h-[300px] object-cover p-3' />
                                                 <h1 className='text-center text-[20px] text-[#383838] font-semibold mb-[30px]'>{item.name}</h1>
                                             </div>
                                         )
